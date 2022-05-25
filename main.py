90844e40978bc4fa9cb5ad7e8b9146ea1ad3c7f8
Дмитрий Гарифулин <garifulin.1309@gmail.com>
2022-05-26 00:52:13 +0700

    Переделана возможность выбора фигуры, оптимизация кода

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-# Импорт библиотеки Pygame, random
+# Импорт библиотек
 import pygame
 from random import*
 import random as rd
+from sys import exit
 
+# разрешение экрана
 widthX = 800
 heightY = 600
 
 screen = pygame.display.set_mode((widthX, heightY))
-pygame.display.set_caption('Выберите фигуру')
+pygame.display.set_caption('Выберите фигуру...')
 
 # кадры в секунду
 FPS = 60
@@ -26,14 +28,22 @@ class Figure:
     color = random_color('self')
 
     # толщина фигур
-    lineTh = 10
+    lineTh = 5
 
 # класс точка наслежует класс фигуры
 class Dot(Figure):
 
+    X = widthX / 2
+    Y = heightY / 2
+
     # метод рисования точки
     def drawDot(self):
-        pygame.draw.circle(screen, Figure.color,(widthX / 2, heightY / 2), Figure.lineTh)
+        pygame.draw.circle(screen, Figure.color,(Dot.X, Dot.Y), Figure.lineTh)
+
+    def set_new_position(self):
+        Dot.X += 1
+        Dot.Y += 1
+        self.draw()
 
     # метод рисования отрезка
     def drawSegment(self):
@@ -64,7 +74,9 @@ class Segment(Dot): # класс линии наследует класс точ
         pygame.draw.rect(screen, Figure.color, pygame.Rect(widthX / 2 - size / 2, heightY / 2 - size / 2, size, size), Figure.lineTh)
 
     def drawTriangle(self):
-        pygame.draw.polygone(screen, Figure.color, points=[(50, 100), (100, 50), (150, 100)])
+        size = 100
+        pygame.draw.polygon(screen, Figure.color, [(widthX / 2 - size, heightY / 2 + size),
+                                                   (widthX / 2 + size, heightY / 2 + size), ((widthX / 2), heightY / 2 - size)], Figure.lineTh)
 
 class Triangle(Segment):
     pass
@@ -83,56 +95,88 @@ rectangle = Rectangle()
 circle = Circle()
 # инициализация элиипса
 ellipse = Ellipse()
+# инициализация треугольника
+triangle = Triangle()
 
-# обработчик событий при нажатии кнопок
-def keys():
+# обработчик событий при нажатии кнопок предварительный просмотр
+def PreviewKeys(choice):
+    if event.type == pygame.KEYDOWN:
 
-    keys = pygame.key.get_pressed()
+        # точка
+        if event.key == pygame.K_1:
+            screen.fill((0, 0, 0))
+            dot.drawDot()
+            pygame.display.set_caption('Выбрана ТОЧКА для подтверждения нажмите [Enter]')
+            choice = 1
 
-    # точка
-    if keys[pygame.K_1]:
-        screen.fill((0, 0, 0))
-        dot.drawDot()
-        pygame.display.set_caption('Выбрана ТОЧКА для подтверждения нажмите [Enter]')
+        # линия
+        if event.key == pygame.K_2:
+            screen.fill((0, 0, 0))
+            dot.drawSegment()
+            pygame.display.set_caption('Выбран ОТРЕЗОК для подтверждения нажмите [Enter]')
+            choice = 2
 
-    # линия
-    if keys[pygame.K_2]:
-        screen.fill((0, 0, 0))
-        dot.drawSegment()
-        pygame.display.set_caption('Выбран ОТРЕЗОК для подтверждения нажмите [Enter]')
+        # прямоугольник
+        if event.key == pygame.K_3:
+            screen.fill((0, 0, 0))
+            rectangle.drawRectangle()
+            pygame.display.set_caption('Выбран ПРЯМОУГОЛЬНИК для подтверждения нажмите [Enter]')
+            choice = 3
 
-    # прямоугольник
-    if keys[pygame.K_3]:
-        screen.fill((0, 0, 0))
-        rectangle.drawRectangle()
-        pygame.display.set_caption('Выбран ПРЯМОУГОЛЬНИК для подтверждения нажмите [Enter]')
+        # окружность
+        if event.key == pygame.K_4:
+            screen.fill((0, 0, 0))
+            circle.drawCircle()
+            pygame.display.set_caption('Выбрана ОКРУЖНОСТЬ для подтверждения нажмите [Enter]')
+            choice = 4
 
-    # окружность
-    if keys[pygame.K_4]:
-        screen.fill((0, 0, 0))
-        circle.drawCircle()
-        pygame.display.set_caption('Выбрана ОКРУЖНОСТЬ для подтверждения нажмите [Enter]')
+        # Эллипс
+        if event.key == pygame.K_5:
+            screen.fill((0, 0, 0))
+            ellipse.drawEllipse()
+            pygame.display.set_caption('Выбран ЭЛЛИПС для подтверждения нажмите [Enter]')
+            choice = 5
+        # Эллипс
+        if event.key == pygame.K_6:
+            screen.fill((0, 0, 0))
+            triangle.drawTriangle()
+            pygame.display.set_caption('Выбран ТРЕУГОЛЬНИК для подтверждения нажмите [Enter]')
+            choice = 6
 
-    # Эллипс
-    if keys[pygame.K_5]:
-        screen.fill((0, 0, 0))
-        ellipse.drawEllipse()
-        pygame.display.set_caption('Выбран ЭЛЛИПС для подтверждения нажмите [Enter]')
+        # выход из программы ESC
+        if event.key == pygame.K_ESCAPE:
+            pygame.quit()
+            exit()
 
-    # выход из программы ESC
-    if keys[pygame.K_ESCAPE]:
-        pygame.quit()
-        exit()
+    return choice
+
+# выбор после предварительного просмотра
+def Change():
+    if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_RETURN:
+
+            if choice == 1:
+                print("Выбрана фигура 1")
+            if choice == 2:
+                print("Выбрана фигура 2")
+            if choice == 3:
+                print("Выбрана фигура 3")
+            if choice == 4:
+                print("Выбрана фигура 4")
+            if choice == 5:
+                print("Выбрана фигура 5")
+            if choice == 6:
+                print("Выбрана фигура 6")
 
 while True:
     clock.tick(FPS)
     for event in pygame.event.get():
-
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
         else:
             # обновление дисплея
             pygame.display.update()
-            # вызов функции события клавиш
-            keys()
+            # возвращаем значение предпросмотра choice
+            choice = PreviewKeys(choice)
+            Change()
